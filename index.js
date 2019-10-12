9abec3f532dde3ecb861c89bd3c5847560b5b973
Aakarsh Chopra <alcatrazz.627@gmail.com>
2019-10-12 13:23:10 +0530

    Basic Read and Delete working with error handling. Reading what methods to enable from schema

--- index.js
+++ index.js
@@ -18,15 +18,62 @@ try {
     let schema = JSON.parse(fs.readFileSync(schemaFile, { encoding: 'utf-8' }))
     // console.log(schema)
     db.defaults({ [schema.model]: [...schema.seed.map(s => ({ _id: uuid4(), ...s }))] }).write()
-    // console.log(db.get(schema.model).value())
+    dbModelRef = db.get(schema.model)
 
-    app.get(`${schema.baseUrl || "/" + schema.model}/`, (req, res) => res.json(db.get(schema.model).value()))
+    let modelBaseURL = `${schema.baseUrl || "/" + schema.model}/`
+
+    // Read
+    if (schema.methods.includes("read")) {
+        app.get(`${modelBaseURL}:id/`, (req, res) => {
+            let obj = dbModelRef.find({ _id: req.params.id }).value()
+            obj ? res.send(obj) : res.sendStatus(404)
+        })
+
+        app.get(modelBaseURL, (req, res) => res.json(dbModelRef.value()))
+    } else {
+        app.get(`${modelBaseURL}:id/`, (req, res) => res.status(405).send("Method does not exist"))
+        app.get(`${modelBaseURL}`, (req, res) => res.status(405).send("Method does not exist"))
+    }
+
+    // Create
+    if (schema.methods.includes("create")) {
+        app.post(modelBaseURL, (req, res) => {
+            // if conforms to schema AND does not already exist for given unique values, add to db. Cannot add primary keys.
+        })
+    } else {
+        app.post(`${modelBaseURL}`, (req, res) => res.status(405).send("Method does not exist"))
+    }
+
+    // Update
+    if (schema.methods.includes("update")) {
+        // TODO
+    } else {
+        app.put(`${modelBaseURL}/:id/`, (req, res) => res.status(405).send("Method does not exist"))
+        app.patch(`${modelBaseURL}/:id/`, (req, res) => res.status(405).send("Method does not exist"))
+    }
+
+    // Delete
+    if (schema.methods.includes("delete")) {
+        app.delete(`${modelBaseURL}:id/`, (req, res) => {
+            // if exists in db, delete.
+            let lookup = dbModelRef.find({ _id: req.params.id }).value()
+            if (lookup) {
+                dbModelRef.remove(d => d._id == req.params.id).write()
+                res.sendStatus(204)
+            } else {
+                res.sendStatus(404)
+
+            }
+        })
+    } else {
+        app.delete(`${modelBaseURL}`, (req, res) => res.status(405).send("Method does not exist"))
+    }
 
     app.listen(PORT, () => {
-        console.log(`REST API started on port ${PORT} for model ${schema.model}.\nEnabled routes: ${(schema.methods.join(", "))}`);
+        console.log(`REST API started on port ${PORT} for model ${schema.model}, at path ${modelBaseURL}.\nEnabled methods: ${(schema.methods.join(", "))}`);
     });
 
 
 } catch (error) {
-    console.log(error)
+    console.error(error)
 }
